cb31c46b3adddb70a54d1e8e82ff2bfcb400e929
smoothie-ws <kibot.email@gmail.com>
2025-01-08 20:37:06 +1000

    Update `s2D`

--- khafile.js
+++ khafile.js
@@ -8,14 +8,12 @@ project.addAssets("assets/**", {
 });
 
 // s2D Compiler Flags
-process.verbose = true;
 process.defines = [
     "S2D_DEBUG_FPS",
     "S2D_RP_ENV_LIGHTING",
-    "S2D_PP",
     "S2D_PP_DOF",
-    "S2D_PP_DOF_QUALITY",
-    "S2D_PP_MIST",
+    // "S2D_PP_DOF_QUALITY 1",
+    // "S2D_PP_MIST",
     "S2D_PP_FISHEYE",
     "S2D_PP_FILTER",
     "S2D_PP_COMPOSITOR",
--- src/Main.hx
+++ src/Main.hx
@@ -1,27 +1,43 @@
 package;
 
-import kha.Scheduler;
 import kha.System;
 import kha.Assets;
+import kha.Scheduler;
+import kha.input.KeyCode;
+import kha.input.Keyboard;
 // s2d
 import s2d.S2D;
 import s2d.objects.Light;
 import s2d.objects.Sprite;
-import s2d.graphics.Filter;
 import s2d.graphics.PostProcessing;
+import s2d.graphics.postprocessing.Filter.Kernel;
 
 using s2d.core.utils.extensions.FastMatrix4Ext;
 
 class Main {
 	public static function main() {
-		System.start({title: "Game", width: 1024, height: 1024}, function(window) {
+		System.start({
+			title: "Game",
+			width: 1024,
+			height: 1024,
+			framebuffer: {verticalSync: false}
+		}, function(window) {
+			Keyboard.get().notify(function(key:KeyCode) {
+				if (key == F11)
+					window.mode = Fullscreen;
+				if (key == F12)
+					window.mode = Windowed;
+				if (key == Escape)
+					System.stop();
+			});
 			S2D.init(window.width, window.height);
 			window.notifyOnResize(S2D.resize);
 
 			S2D.scale = 1;
 
-			#if S2D_PP
-			PostProcessing.filters.push(Filter.BoxBlur);
+			#if S2D_PP_FILTER
+			for (i in 0...32)
+				PostProcessing.filter.addKernel(Kernel.GaussianBlur);
 			#if S2D_PP_FISHEYE
 			PostProcessing.fisheye.strength = 0.0;
 			#end
@@ -30,14 +46,16 @@ class Main {
 			Assets.loadEverything(function() {
 				S2D.compile();
 
+				#if S2D_RP_ENV_LIGHTING
 				S2D.stage.environmentMap = Assets.images.environment;
+				#end
 
 				var sprite = new Sprite();
 				sprite.material.colorMap = Assets.images.color;
 				sprite.material.normalMap = Assets.images.normal;
 				sprite.material.ormMap = Assets.images.orm;
 				sprite.material.glowMap = Assets.images.glow;
-				sprite.material.glowStrength = 0.0;
+				sprite.material.glowStrength = 1.0;
 				sprite.material.depthScale = 1.0;
 
 				var light = new Light();
