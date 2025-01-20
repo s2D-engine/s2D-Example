e2aa0a061498666f5443dfd1f409d907db5c2292
smoothie-ws <kibot.email@gmail.com>
2025-01-21 00:59:17 +1000

    Update example with shadows

--- khafile.js
+++ khafile.js
@@ -10,11 +10,15 @@ project.addAssets("assets/**", {
 // s2D Compiler Flags
 process.defines = [
     "S2D_DEBUG_FPS",
-    "S2D_RP_ENV_LIGHTING",
+    "S2D_SPRITE_INSTANCING",
+    "S2D_LIGHTING",
+    "S2D_LIGHTING_SHADOWS",
+    "S2D_LIGHTING_DEFERRED",
+    // "S2D_LIGHTING_ENVIRONMENT",
     // "S2D_PP_BLOOM",
     // "S2D_PP_FISHEYE",
     // "S2D_PP_FILTER",
-    // "S2D_PP_COMPOSITOR",
+    // "S2D_PP_COMPOSITOR"
 ];
 await project.addProject("s2D");
 
--- src/Main.hx
+++ src/Main.hx
@@ -1,22 +1,20 @@
 package;
 
-import s2d.Layer;
+import haxe.ds.Vector;
 import kha.System;
 import kha.Assets;
 import kha.input.KeyCode;
+import kha.math.FastVector2;
 // s2d
 import s2d.S2D;
+import s2d.Layer;
+import s2d.SpriteAtlas;
 import s2d.core.Time;
-import s2d.core.Timer;
 import s2d.core.Input;
-import s2d.math.SMath;
+import s2d.core.Timer;
 import s2d.objects.Light;
 import s2d.objects.Sprite;
-import s2d.animation.Easing;
-import s2d.animation.Action;
-import s2d.graphics.PostProcessing;
-import s2d.graphics.postprocessing.Filter;
-import s2d.events.Dispatcher;
+import s2d.objects.StageObject;
 
 class Main {
 	public static function main() {
@@ -24,102 +22,91 @@ class Main {
 			title: "Game",
 			width: 1024,
 			height: 1024,
-			framebuffer: {verticalSync: false}
+			framebuffer: {
+				verticalSync: false
+			}
 		}, function(window) {
-			S2D.init(window.width, window.height);
+			S2D.ready(window.width, window.height);
 			window.notifyOnResize(S2D.resize);
 
-			Input.keyboard.notify(function(key:KeyCode) {
-				switch (key) {
-					case F11:
-						window.mode = Fullscreen;
-					case F12:
-						window.mode = Windowed;
-					case Escape:
-						System.stop();
-					default:
-						null;
-				}
-			});
-
-			S2D.scale = 1.5;
-
-			#if S2D_PP_MIST
-			PostProcessing.mist.color = Black;
-			#end
-			#if S2D_PP_BLOOM
-			PostProcessing.bloom.radius = 4.0;
-			PostProcessing.bloom.intensity = 0.75;
-			PostProcessing.bloom.threshold = 0.25;
-			#end
-			#if S2D_PP_FILTER
-			PostProcessing.filter.addKernel(Filter.Sharpen);
-			#end
-			#if S2D_PP_FISHEYE
-			PostProcessing.fisheye.strength = -1.0;
-			#end
-			#if S2D_PP_COMPOSITOR
-			PostProcessing.compositor.vignetteStrength = 0.0;
-			#end
+			S2D.scale = 2.5;
 
 			Assets.loadEverything(function() {
-				S2D.compile();
+				S2D.set();
 
-				#if (S2D_RP_ENV_LIGHTING == 1)
+				#if (S2D_LIGHTING_ENVIRONMENT == 1)
 				S2D.stage.environmentMap = Assets.images.environment;
 				#end
 
 				var layer = new Layer();
+				var atlas = new SpriteAtlas(layer);
+				#if (S2D_LIGHTING == 1)
+				atlas.albedoMap = Assets.images.albedo;
+				atlas.normalMap = Assets.images.normal;
+				atlas.ormMap = Assets.images.orm;
+				atlas.emissionMap = Assets.images.emission;
+				#else
+				atlas.textureMap = Assets.images.albedo;
+				#end
 
-				var sprite = new Sprite(layer);
-				sprite.albedoMap = Assets.images.albedo;
-				sprite.normalMap = Assets.images.normal;
-				sprite.ormMap = Assets.images.orm;
-				sprite.emissionMap = Assets.images.emission;
+				#if (S2D_LIGHTING_SHADOWS == 1)
+				var sv = new Vector(4);
+				sv[0] = new FastVector2(-0.5, -0.6);
+				sv[1] = new FastVector2(-0.5, 0.6);
+				sv[2] = new FastVector2(0.5, 0.6);
+				sv[3] = new FastVector2(0.5, -0.6);
+				#end
 
-				var d = 0.50;
-				var speed = 2.5;
-				Input.keyboard.notify(function(key) {
-					if (key == W) {
-						Action.tween(d, (f) -> {
-							sprite.moveG(0.0, -speed * Time.delta * f);
-						}).ease(Easing.OutCubic);
-					}
-					if (key == S) {
-						Action.tween(d, (f) -> {
-							sprite.moveG(0.0, speed * Time.delta * f);
-						}).ease(Easing.OutCubic);
-					}
-					if (key == A) {
-						Action.tween(d, (f) -> {
-							sprite.moveG(-speed * Time.delta * f, 0.0);
-						}).ease(Easing.OutCubic);
-					}
-					if (key == D) {
-						Action.tween(d, (f) -> {
-							sprite.moveG(speed * Time.delta * f, 0.0);
-						}).ease(Easing.OutCubic);
-					}
-				});
+				var sprite1 = new Sprite(atlas);
+				sprite1.moveG(-1.0);
+				new Timer(function() {
+					sprite1.rotateG(0.01);
+				}, 1 / 60).repeat(10000);
+				#if (S2D_LIGHTING_SHADOWS == 1)
+				sprite1.shadowVertices = sv;
+				#end
+
+				var sprite2 = new Sprite(atlas);
+				sprite2.moveG(1.0);
+				new Timer(function() {
+					sprite2.rotateG(-0.01);
+				}, 1 / 60).repeat(10000);
+				#if (S2D_LIGHTING_SHADOWS == 1)
+				sprite2.shadowVertices = sv;
+				#end
 
 				var light = new Light(layer);
-				light.color = Color.fromFloats(0.9, 0.9, 0.5);
-				light.radius = 1.0;
-				light.power = 10;
-				light.z = 0.1;
-				light.volume = 0.05;
-
-				Input.mouse.notify(null, null, function(x, y, mx, my) {
-					var p = S2D.screen2WorldSpace({x: x, y: y});
-					light.moveToG(p.xy);
-				}, function(delta) {
-					Action.tween(d, (f) -> {
-						f = 0.1 + 1.0 - abs(f - 0.5) * 2.0;
-						sprite.rotation += f * delta * Time.delta;
-					}).ease(Easing.OutCubic);
+				light.color = Color.fromFloats(Math.random(), Math.random(), Math.random());
+				light.radius = 1;
+				light.power = 100;
+				light.z = 1.0;
+				#if (S2D_LIGHTING_DEFERRED == 1)
+				light.volume = 0.1;
+				#end
+				light.moveG({
+					x: (Math.random() * 2.0 - 1.0),
+					y: (Math.random() * 2.0 - 1.0)
 				});
 
 				S2D.stage.layers = [layer];
+
+				Input.mouse.notify(null, null, function(x, y, mx, my) {
+					var p = S2D.screen2WorldSpace({x: x, y: y});
+					light.x = p.x;
+					light.y = p.y;
+				});
+				Input.keyboard.notify(function(key:KeyCode) {
+					switch (key) {
+						case F11:
+							window.mode = Fullscreen;
+						case F12:
+							window.mode = Windowed;
+						case Escape:
+							System.stop();
+						default:
+							null;
+					}
+				});
 
 				System.notifyOnFrames(function(frames) {
 					S2D.render(frames[0]);
